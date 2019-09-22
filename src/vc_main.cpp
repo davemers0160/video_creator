1a041e9c274ede1a495c2641d8ac4dd09de0350a
Dave <davemers@iu.edu>
2019-09-21 23:29:58 -0400

    move directory listing to it's own file
    still needs linux testing

--- src/vc_main.cpp
+++ src/vc_main.cpp
@@ -2,6 +2,8 @@
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
 #include <windows.h>
+#elif defined(__linux__) 
+
 #endif
 
 #include <cstdint>
@@ -17,6 +19,7 @@
 //#include <fstream>
 
 #include "file_parser.h"
+#include "get_directory_listing.h"
 
 // OpenCV includes
 #include <opencv2/core.hpp>
@@ -26,58 +29,58 @@
 //#include <opencv2/imgproc/imgproc.hpp>  
 #include <opencv2/videoio.hpp>
 
-
-// ----------------------------------------------------------------------------------------
-// https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
-std::vector<std::string> get_directory_listing(std::string folder)
-{
-    std::vector<std::string> file_names;
-    std::string search_path = folder + "*.*";
-
-    WIN32_FIND_DATA fd;
-    void* hFind = ::FindFirstFile(search_path.c_str(), &fd);
-
-#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-    if (hFind != INVALID_HANDLE_VALUE)
-    {
-        do 
-        {
-            // read all (real) files in current folder
-            // , delete '!' read other 2 default folder . and ..
-            if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-                file_names.push_back(fd.cFileName);
-            }
-        } while (::FindNextFile(hFind, &fd));
-        ::FindClose(hFind);
-    }
-#else
-    DIR* dir;
-    class dirent* ent;
-    class stat st;
-
-    dir = opendir(directory.c_str());
-    while ((ent = readdir(dir)) != NULL) {
-        const string file_name = ent->d_name;
-        const string full_file_name = directory + "/" + file_name;
-
-        if (file_name[0] == '.')
-            continue;
-
-        if (stat(full_file_name.c_str(), &st) == -1)
-            continue;
-
-        const bool is_directory = (st.st_mode & S_IFDIR) != 0;
-
-        if (is_directory)
-            continue;
-
-        file_names.push_back(file_name); // returns just filename
-    }
-    closedir(dir);
-#endif
-
-    return file_names;
-}
+//
+//// ----------------------------------------------------------------------------------------
+//// https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
+//std::vector<std::string> get_directory_listing(std::string folder)
+//{
+//    std::vector<std::string> file_names;
+//    std::string search_path = folder + "*.*";
+//
+//    WIN32_FIND_DATA fd;
+//    void* hFind = ::FindFirstFile(search_path.c_str(), &fd);
+//
+//#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+//    if (hFind != INVALID_HANDLE_VALUE)
+//    {
+//        do 
+//        {
+//            // read all (real) files in current folder
+//            // , delete '!' read other 2 default folder . and ..
+//            if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+//                file_names.push_back(fd.cFileName);
+//            }
+//        } while (::FindNextFile(hFind, &fd));
+//        ::FindClose(hFind);
+//    }
+//#else
+//    DIR* dir;
+//    class dirent* ent;
+//    class stat st;
+//
+//    dir = opendir(directory.c_str());
+//    while ((ent = readdir(dir)) != NULL) {
+//        const string file_name = ent->d_name;
+//        const string full_file_name = directory + "/" + file_name;
+//
+//        if (file_name[0] == '.')
+//            continue;
+//
+//        if (stat(full_file_name.c_str(), &st) == -1)
+//            continue;
+//
+//        const bool is_directory = (st.st_mode & S_IFDIR) != 0;
+//
+//        if (is_directory)
+//            continue;
+//
+//        file_names.push_back(file_name); // returns just filename
+//    }
+//    closedir(dir);
+//#endif
+//
+//    return file_names;
+//}
 
 // ----------------------------------------------------------------------------------------
 void print_help()
